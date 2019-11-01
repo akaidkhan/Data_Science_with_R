9752fa658e1efe33b6110c3a58ca2d4a876c65ce
Akaid Khan <43861949+akaidkhan@users.noreply.github.com>
2019-11-01 07:42:58 -0700

    R Import Export

---
+++ R Import  Export/R_CSV_File_Import.R
@@ -0,0 +1,18 @@
+# Set the directory to the folder 
+getwd()
+setwd("F:\\data science\\Data_Science_with_R\\R Import  Export")
+
+####################################################################################
+#Importing csv file
+# read.csv()
+titanic_train<- read.csv(file.choose())
+class(titanic_train)
+
+titanic <- read.csv("titanic_train.csv")
+str(titanic)
+
+#Using readr package
+install.packages("readr")
+library(readr)
+titanic <- read_csv("titanic_train.csv")
+titanic
