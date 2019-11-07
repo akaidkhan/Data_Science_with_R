ecc22106f4756afff390b6228c63d2c1bc2ed5ee
Akaid Khan <43861949+akaidkhan@users.noreply.github.com>
2019-11-07 10:02:30 -0800

    Data Visualization

---
+++ R Data Visualization/R_data_visualization_density_Plot.R
@@ -0,0 +1,26 @@
+###############################################################################
+
+
+
+#Kernel Density Plot
+
+
+
+density_data <- density(mtcars$mpg)
+
+
+
+plot(density_data)
+
+
+
+plot(density_data, main = "Kernel Density of Miles per gallon")
+
+
+
+polygon(density_data, col = "skyblue", border= "black" )
+
+
+
+###############################################################################
+
